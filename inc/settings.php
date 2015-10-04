53cf0bedef6f8efb7e43ae13d82473d3034cab62
Rodrigo Brito <rig.10@hotmail.com>
2015-10-03 23:37:05 -0300

    Inclusão do ACF e arquivo de configurações

--- inc/settings.php
+++ inc/settings.php
@@ -15,7 +15,7 @@ function destaque_cpt() {
 
     $slider->set_arguments(
         array(
-            'supports' => array( 'title', 'editor', 'thumbnail' ),
+            'supports' => array( 'title', 'thumbnail' ),
             'menu_position' => 5,
             'menu_icon' => 'dashicons-images-alt'
         )
@@ -39,7 +39,7 @@ function programacao_cpt() {
 
     $programacao->set_arguments(
         array(
-            'supports' => array( 'title', 'editor', 'thumbnail' ),
+            'supports' => array( 'title', 'thumbnail' ),
             'menu_position' => 6,
             'menu_icon' => 'dashicons-calendar-alt'
         )
@@ -70,3 +70,157 @@ function evento_taxonomy() {
 }
 
 add_action( 'init', 'evento_taxonomy', 1 );
+
+/**
+ *  Advanced Custom Fields - ACF
+ */
+
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+ 
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = get_stylesheet_directory() . '/inc/acf/';
+    
+    // return
+    return $path;
+    
+}
+ 
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/inc/acf/';
+    
+    // return
+    return $dir;
+    
+}
+ 
+
+// 3. Hide ACF field group menu item
+//add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/inc/acf/acf.php' );
+
+
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_eventos-fields',
+		'title' => 'Eventos Fields',
+		'fields' => array (
+			array (
+				'key' => 'field_561089250710a',
+				'label' => 'Data',
+				'name' => 'data_evento',
+				'type' => 'date_picker',
+				'instructions' => 'Data de acontecimento do evento',
+				'date_format' => 'yymmdd',
+				'display_format' => 'dd/mm/yy',
+				'first_day' => 1,
+			),
+			array (
+				'key' => 'field_56108a66d90c2',
+				'label' => 'Hora',
+				'name' => 'hora_evento',
+				'type' => 'text',
+				'instructions' => 'Hora de acontecimento do evento (Opcional)',
+				'default_value' => '',
+				'placeholder' => 'Ex: 12:00',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+			array (
+				'key' => 'field_56108afb7deb5',
+				'label' => 'Descrição do evento',
+				'name' => 'descricao_evento',
+				'type' => 'textarea',
+				'instructions' => 'Breve descrição sobre o evento',
+				'default_value' => '',
+				'placeholder' => 'Descrever evento...',
+				'maxlength' => 254,
+				'rows' => '',
+				'formatting' => 'none',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'evento',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'acf_after_title',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+	register_field_group(array (
+		'id' => 'acf_slider-fields',
+		'title' => 'Slider Fields',
+		'fields' => array (
+			array (
+				'key' => 'field_56108d55ad119',
+				'label' => 'Link do Slider',
+				'name' => 'slider_link',
+				'type' => 'post_object',
+				'instructions' => 'Link para página ou conteúdo interno relacionado ao slider',
+				'post_type' => array (
+					0 => 'all',
+				),
+				'taxonomy' => array (
+					0 => 'all',
+				),
+				'allow_null' => 0,
+				'multiple' => 0,
+			),
+			array (
+				'key' => 'field_56108e2ce59bb',
+				'label' => 'Descrição do Slider',
+				'name' => 'slider_descricao',
+				'type' => 'textarea',
+				'instructions' => 'Breve descrição que será exibida ao passar o Slider',
+				'default_value' => '',
+				'placeholder' => 'Descrever slider...',
+				'maxlength' => 260,
+				'rows' => '',
+				'formatting' => 'br',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'slider',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'acf_after_title',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}

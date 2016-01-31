2eabf4e2c7d2285983d729e87d1d33d0a1b6a503
Rodrigo Brito <rig.10@hotmail.com>
2016-01-31 21:12:01 -0200

    Alterações na cor principal e separação do slider

--- home.php
+++ home.php
@@ -17,51 +17,7 @@ get_header(); ?>
 
 	</div>
 </div>
-<?php
-$args = array(
-	'numberposts'=>5,            // should show 5 but only shows 3
-	'post_type'=>'post',         // posts only
-	'meta_key'=>'_thumbnail_id', // with thumbnail
-);
-$sliders = get_posts( $args ); ?>
-<div id="carousel-home" class="carousel slide carousel-fade" data-ride="carousel">
-	<!-- Indicators -->
-	<?php if($sliders): ?>
-	<ol class="carousel-indicators">
-	<?php for($i = 0; $i < count($sliders); $i++): ?>
-		<li data-target="#carousel-home" data-slide-to="<?php echo $i; ?>" class="<?php echo $i==0?"active":""; ?>"></li>
-	<?php endfor; ?>
-	</ol>
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner" role="listbox">
-		<?php
-			$active_class = "active";
-			foreach ( $sliders as $post ) : setup_postdata( $post );
-				$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
-					<div class="item <?php echo $active_class; ?>" style="background-image: url(<?php echo $large_image_url[0]; ?>);">
-					  <div class="carousel-caption">
-						<h1><?php the_title(); ?></h1>
-						<p><?php the_excerpt(); ?></p>
-					  </div>
-					</div>
-				<?php
-				$active_class = "";
-			endforeach;
-			wp_reset_postdata();
-		?>
-	</div>
-
-	<!-- Controls -->
-	<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
-	<?php endif; ?>
-</div>
+<?php get_template_part('slider'); ?>
 <div id="wrapper" class="container">
 	<div class="row">
 		<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
---
+++ slider.php
@@ -0,0 +1,47 @@
+<?php
+$args = array(
+	'numberposts'=>5,
+	'post_type'=>'post',
+	'meta_key'=>'_thumbnail_id',
+);
+$sliders = get_posts( $args ); ?>
+<div id="carousel-home" class="carousel slide carousel-fade" data-ride="carousel">
+	<!-- Indicators -->
+	<?php if($sliders): ?>
+	<ol class="carousel-indicators">
+	<?php for($i = 0; $i < count($sliders); $i++): ?>
+		<li data-target="#carousel-home" data-slide-to="<?php echo $i; ?>" class="<?php echo $i==0?"active":""; ?>"></li>
+	<?php endfor; ?>
+	</ol>
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<?php
+			$active_class = "active";
+			foreach ( $sliders as $post ) : setup_postdata( $post );
+				$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+					<div class="item <?php echo $active_class; ?>" style="background-image: url(<?php echo $large_image_url[0]; ?>);">
+						<a href="<?php the_permalink(); ?>">
+						  <div class="carousel-caption">
+							<h1><?php the_title(); ?></h1>
+							<p><?php the_excerpt(); ?></p>
+						  </div>
+						</a>
+					</div>
+				<?php
+				$active_class = "";
+			endforeach;
+			wp_reset_postdata();
+		?>
+	</div>
+
+	<!-- Controls -->
+	<a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+	<?php endif; ?>
+</div>

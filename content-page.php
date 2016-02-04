47a8bda805ec5754788960f34842b0288e101cbd
Rodrigo Brito <rig.10@hotmail.com>
2016-02-04 17:29:15 -0200

    changes in description and page padding

--- content-page.php
+++ content-page.php
@@ -2,15 +2,17 @@
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
-	<div class="entry-content">
-		<?php
-			the_content();
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'morfeu' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	<div class="conteudo-post">
+		<div class="entry-content">
+			<?php
+				the_content();
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'morfeu' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div><!-- .conteudo-post -->
 </article><!-- #post-## -->
